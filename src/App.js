2d5b68898aac22b3c8ed83494a3dae76978e545f
Filipp Gudis <fgudis@yahoo.com>
2020-12-20 23:57:50 -0800

    animating lists at work, transitionGroup element

--- src/App.js
+++ src/App.js
@@ -6,20 +6,16 @@ import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
-const animationTime = {   // this can define our different timing for enetering and exiting!!! 
-  enter: 400,
-  exit: 1000
-};
-
 class App extends Component {
   state = {
     modalIsOpen: false,
-    showBlock: false,
+    showBlock: false
   };
 
   showModal = () => {
     this.setState({ modalIsOpen: true });
   };
+
   closeModal = () => {
     this.setState({ modalIsOpen: false });
   };
@@ -31,24 +27,24 @@ class App extends Component {
         <button
           className="Button"
           onClick={() =>
-            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
-          }
+            this.setState(prevState => ({ showBlock: !prevState.showBlock }))}
         >
           Toggle
         </button>
         <br />
-        {/* {this.state.showBlock ? ( */}
-        {/* transition element gives us 4 states (entering, entered, exiting, exited)
-       of our element that we can control 
-       depending on what stage we are in our transition
-       this way we can control our DOM and still keep the animation using TRANSITION ELEMENT  */}
         <Transition
           in={this.state.showBlock}
-          timeout={animationTime}
-          mountOnEnter //TELLS US TO MOUNT OUR ELEMENT TO DOM ON ENTERING STAGE OF TRANSITION
-          unmountOnExit // UNMOUNT OUR ELEMENT ONLY AFTER OUR TIMEOUT IS DONE
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
         >
-          {(state) => (
+          {state => (
             <div
               style={{
                 backgroundColor: "red",
@@ -56,23 +52,13 @@ class App extends Component {
                 height: 100,
                 margin: "auto",
                 transition: "opacity 1s ease-out",
-                opacity: state === "exiting" ? 0 : 1,
+                opacity: state === "exiting" ? 0 : 1
               }}
             />
           )}
         </Transition>
-        <Transition
-          mountOnEnter
-          unmountOnExit
-          in={this.state.modalIsOpen}
-          timeout={animationTime}
-        >
-          {(state) => <Modal show={state} closed={this.closeModal} />}
-        </Transition>
-        {/* ) : null} */}
-        {/* conditional rendering will help with reactiveness, but limits our animations/effects
-        because after rendering element, it gets cleared out from the DOM and we dont have an element to apply our closing animation in this case */}
-        {this.state.modalIsOpen ? <Backdrop show /> : null};
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Transition } from 'react-transition-group';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './List.css';
 
@@ -25,19 +28,25 @@ class List extends Component {
 
     render () {
         const listItems = this.state.items.map( (item, index) => (
-            <li 
-                key={index}
+           <CSSTransition    // transitionGroup sets 'IN' property to our csstransition/transition elements automatically! handles lists of items
+           key={index}
+           classNames="fade" timeout={300}>
+                <li 
                 className="ListItem" 
                 onClick={() => this.removeItemHandler(index)}>{item}</li>
+           </CSSTransition>
+            
         ) );
 
         return (
             <div>
                 <button className="Button" onClick={this.addItemHandler}>Add Item</button>
                 <p>Click Item to Remove.</p>
-                <ul className="List">
+               <TransitionGroup 
+               component="ul"
+               className="List">
                     {listItems}
-                </ul>
+                    </TransitionGroup>
             </div>
         );
     }
